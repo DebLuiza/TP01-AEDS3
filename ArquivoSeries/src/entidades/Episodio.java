d7d3d60106615660e79bb9ade81ec7e2cf063180
Deb Luiza <99653788+DebLuiza@users.noreply.github.com>
2025-03-20 19:13:31 -0300

    Primeiro commit com alterações

--- ArquivoSeries/src/entidades/Episodio.java
+++ ArquivoSeries/src/entidades/Episodio.java
@@ -39,11 +39,11 @@ public class Episodio implements EntidadeArquivo {
     }
 
     // Getters e Setters
-    public int getId() {
+    public int getID() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setID(int id) {
         this.id = id;
     }
 
